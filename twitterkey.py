a03693c0797eae2d3ffef26f033546a8ec40a2c8
amanda8412383 <amanda8412383yahoo@gmail.com>
2019-04-11 23:30:13 +0800

    tutorial with git bq import py added ceo.sentiment start

---
+++ twitterkey.py
@@ -0,0 +1,5 @@
+consumer_key = '3H70QQ9AzguYY5KWXmHPO9Qzi'
+consumer_secret = 'J7Pr45oEcpHHosdnQyOytHf8Sn30cZowc5bUyIBhwZTboQeh8W'
+access_token = '991551756889870341-VtFhYuXuggtMNRAg26bVykdoy7Xi6mV'
+access_secret = 'hbtF96dFDxQcNATEtnBOkT5iVNfUK7BcWnGKHktd6W9gZ'
+   
